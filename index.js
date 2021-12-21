aa1088ae8b63e795dd5dd3a336a9ab0a5b3e078c
Christopher Terrano <mterrano1@yahoo.com>
2021-12-21 13:34:15 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function createEmployeeRecord(array){
+    const employeeRecord = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+    return employeeRecord
+}
+
+function createEmployeeRecords(arrays){
+    arrays.map(createEmployeeRecord)
+}
