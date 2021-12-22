be6f73e5654c4e384618a3af97d3324149117838
Christopher Terrano <mterrano1@yahoo.com>
2021-12-22 10:23:51 -0600

    Done.

--- index.js
+++ index.js
@@ -17,11 +17,48 @@ function createEmployeeRecords(arrays){
 }
 
 function createTimeInEvent(object, dateStamp){
+    let [date, hour] = dateStamp.split(' ')
     const timeObj = {
     type: 'TimeIn',
-    hour: dateStamp.slice(11, 15),
-    date: dateStamp.slice(0, 10)
+    hour: parseInt(hour),
+    date: date
 }
 object.timeInEvents.push(timeObj)
 return object
+}
+
+function createTimeOutEvent(object, dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+    const timeObj = {
+        type: 'TimeOut',
+        hour: parseInt(hour),
+        date: date
+    }
+    object.timeOutEvents.push(timeObj)
+    return object
+}
+
+function hoursWorkedOnDate(object, date){
+    let findTimeInDate = object.timeInEvents.find(obj => obj.date === date);
+    let startTime = findTimeInDate.hour;
+    let findTimeOutDate = object.timeOutEvents.find(obj => obj.date === date);
+    let endTime = findTimeOutDate.hour;
+    return (endTime - startTime) / 100
+}
+
+function wagesEarnedOnDate(object, date){
+    let hoursWorked = hoursWorkedOnDate(object, date);
+    return hoursWorked * object.payPerHour
+}
+
+function allWagesFor(object){
+    let allTimeInDates = object.timeInEvents.map(findDates => findDates.date);
+    let allTimeOutDates = object.timeOutEvents.map(findDates => findDates.date);
+
+
+    allTimeInDates.forEach(date => wagesEarnedOnDate(object, date))
+
+
+//     console.log(allTimeInDates)
+//     console.log(allTimeOutDates)
 }
